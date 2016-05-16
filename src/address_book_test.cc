7304d34232dbeeb8468feee5d28efa360959c579
Łukasz Bieniasz-Krzywiec <lukaszbk@gmail.com>
2016-05-15 20:38:33 -0700

    Settle on using gcc-4.6 for now.

--- src/address_book_test.cc
+++ src/address_book_test.cc
@@ -4,15 +4,13 @@
 using ::src::AddressBook;
 using ::src::Person;
 
-using PhoneNumber = Person::PhoneNumber;
-
 int main() {
   AddressBook address_book;
   Person* person = address_book.add_person();
   person->set_name("Bajtazar");
   person->set_id(42);
   person->set_email("bajtazar@gmail.com");
-  PhoneNumber* phone = person->add_phone();
+  Person::PhoneNumber* phone = person->add_phone();
   phone->set_number("(555) 123-4567");
   phone->set_type(Person::MOBILE);
 

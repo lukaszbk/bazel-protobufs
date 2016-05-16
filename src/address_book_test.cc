f9de290bd6632c38d45c5e82b018b8b4df11c2ed
Łukasz Bieniasz-Krzywiec <lukaszbk@gmail.com>
2016-05-15 19:27:55 -0700

    Add a simple test to make sure that cc_proto_library build rule works.

---
+++ src/address_book_test.cc
@@ -0,0 +1,20 @@
+#include "src/address_book.pb.h"
+#include "src/person.pb.h"
+
+using ::src::AddressBook;
+using ::src::Person;
+
+using PhoneNumber = Person::PhoneNumber;
+
+int main() {
+  AddressBook address_book;
+  Person* person = address_book.add_person();
+  person->set_name("Bajtazar");
+  person->set_id(42);
+  person->set_email("bajtazar@gmail.com");
+  PhoneNumber* phone = person->add_phone();
+  phone->set_number("(555) 123-4567");
+  phone->set_type(Person::MOBILE);
+
+  return !(address_book.person(0).phone(0).type() == Person::MOBILE);
+}

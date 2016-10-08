76c37d100d8b4852fa9064ab4a6c929d924c8103
Łukasz Bieniasz-Krzywiec <lukaszbk@gmail.com>
2016-10-07 22:18:59 -0700

    Check if the compiler in Travis-CI container supports C++11 features.

--- src/address_book_test.cc
+++ src/address_book_test.cc
@@ -4,15 +4,16 @@
 using ::src::AddressBook;
 using ::src::Person;
 
+using PhoneNumber = Person::PhoneNumber;
+
 int main() {
   AddressBook address_book;
   Person* person = address_book.add_person();
   person->set_name("Bajtazar");
   person->set_id(42);
   person->set_email("bajtazar@gmail.com");
-  Person::PhoneNumber* phone = person->add_phone();
+  PhoneNumber* phone = person->add_phone();
   phone->set_number("(555) 123-4567");
   phone->set_type(Person::MOBILE);
-
   return !(address_book.person(0).phone(0).type() == Person::MOBILE);
 }
